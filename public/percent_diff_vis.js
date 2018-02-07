d8c3fa2ed896f7094c203aac95b0dfe0d403075b
Thomas Munoz <thomas.munoz30@gmail.com>
2018-02-07 16:46:00 +0100

    Add property selection

--- public/percent_diff_vis.js
+++ public/percent_diff_vis.js
@@ -1,21 +1,40 @@
 import { VisTypesRegistryProvider } from 'ui/registry/vis_types';
 import { TemplateVisTypeProvider } from 'ui/template_vis_type/template_vis_type';
+import { VisSchemasProvider } from 'ui/vis/schemas';
 import percentDiffTemplate from 'plugins/kibana-percent-diff/percent_diff_vis.html';
-import 'plugins/kibana-percent-diff/percent_diff_vis_controller';
+
+define(function (require) {
+  require('plugins/kibana-percent-diff/percent_diff_vis_controller');
+
+  VisTypesRegistryProvider.register(PercentDiffVisProvider);
+
+  function PercentDiffVisProvider(Private) {
+    const PercentDiffVisType = Private(TemplateVisTypeProvider);
+    const Schemas = Private(VisSchemasProvider);
 
 
+    return new PercentDiffVisType({
+      name: 'percent-diff',
+      title: 'Percent difference',
+      description: 'Percent difference between two dates',
+      icon: 'fa-area-chart',
+      requiresIndexPatternSelection: true,
+      template: percentDiffTemplate,
+      schemas: new Schemas([
+        {
+          group: 'metrics',
+          name: 'metric',
+          title: 'Element to compare',
+          min: 1,
+          max: 1,
+          aggFilter: ['avg'],
+          defaults: [
+            { type: 'avg', schema: 'metric' }
+          ]
+        }
+      ])
+    });
+  }
 
-VisTypesRegistryProvider.register(PercentDiffVisProvider);
+});
 
-function PercentDiffVisProvider(Private) {
-  const PercentDiffVisType = Private(TemplateVisTypeProvider);
-
-  return new PercentDiffVisType({
-    name: 'percent-diff',
-    title: 'Percent difference',
-    description: 'Percent difference between two dates',
-    icon: 'fa-area-chart',
-    requiresIndexPatternSelection: true,
-    template: percentDiffTemplate
-  });
-}
